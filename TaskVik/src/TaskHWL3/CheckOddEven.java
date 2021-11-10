5a4ef356d6355a12b46e1d1031cdf5f65b179b98
Volodymyr Varkhol <75792510+proteriusgrid@users.noreply.github.com>
2021-11-10 21:48:17 +0200

    Add files via upload

---
+++ TaskVik/src/TaskHWL3/CheckOddEven.java
@@ -0,0 +1,19 @@
+package TaskHWL3;
+
+import java.util.Scanner;
+
+public class CheckOddEven {
+    public static void main( String[] args ) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Number: ");
+        int number=sc.nextInt();
+
+//        int number=26;
+
+        if (number % 2 == 0){
+            System.out.println(number + " Even Number " + "BYE");
+        } else {
+            System.out.println(number + " Odd Numbe " + "BYE");
+        }
+    }
+}

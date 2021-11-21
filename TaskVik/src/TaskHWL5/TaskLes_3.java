dbed4a280696cfefd13b896c6da78c851f66fe30
Volodymyr Varkhol <75792510+proteriusgrid@users.noreply.github.com>
2021-11-21 16:31:59 +0200

    proteriusgrid added edits to programs 1.0.3

--- TaskVik/src/TaskHWL5/TaskLes_3.java
+++ TaskVik/src/TaskHWL5/TaskLes_3.java
@@ -1,5 +1,5 @@
 package TaskHWL5;
-import java.io.*;
+import java.util.Scanner;
 
 
 public class TaskLes_3 {
@@ -7,12 +7,26 @@ public class TaskLes_3 {
     {
 
         // character whose ASCII value to be found
-        char ch1 = 'a';
-        char ch2 = 'A';
-        // variable that stores the integer value of the character
-        int asciivalue1 = ch1;
-        int asciivalue2 = ch2;
-        System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
-        System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);
+//        char ch1 = 'a';
+//        char ch2 = 'A';
+//        // variable that stores the integer value of the character
+//        int asciivalue1 = ch1;
+//        int asciivalue2 = ch2;
+//        System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
+//        System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        String value= scanner.next();
+//       //Convert string to a char array.
+        char[] array = value.toCharArray();
+
+//Loop over chars in the array.
+        for(Character c : array) {
+
+            System.out.println(c + 0);
+            System.out.println( (char)(c + 0) );
+        }
     }
 }
